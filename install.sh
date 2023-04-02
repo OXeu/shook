25216b99e789df87cedb8ab7d11feab0d1cbab4d
Xeu <thankrain@qq.com>
2023-04-02 15:37:17 +0800

    [feat] support service daemon
    Signed-off-by: Xeu <thankrain@qq.com>

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -e
 echo "Searching latest version..."
 RELEASES_URL="https://github.com/ThankRain/shook/releases"
+SERVICE_URL="https://raw.githubusercontent.com/ThankRain/shook/main/install/shook.service"
 BASENAME="shook"
 LATEST="$(curl -sI https://github.com/ThankRain/shook/releases/latest | grep location: | awk '{printf $2}' | cut -d '/' -f8 | tr -d '\r\n')"
 
@@ -21,12 +22,17 @@ export TAR_FILE="$TMPDIR/${BASENAME}_$(uname -s)_$(uname -m).tar.gz"
 	echo "$RELEASES_URL/download/$VERSION/${BASENAME}_$(uname -s)_$(uname -m).tar.gz"
 	curl -fLo "$TAR_FILE" \
 		"$RELEASES_URL/download/$VERSION/${BASENAME}_$(uname -s)_$(uname -m).tar.gz"
-	curl -fLo "checksums.txt" "$RELEASES_URL/download/$VERSION/checksums.txt"
+	echo "Downloading checksums.txt"
+	curl -sfLo "checksums.txt" "$RELEASES_URL/download/$VERSION/checksums.txt"
+	echo "Downloading systemd service config"
+	curl -sfLo "shook.service" "$SERVICE_URL"
 	echo "Verifying checksums..."
 	sha256sum --ignore-missing --quiet --check checksums.txt
 )
 tar -xf "$TAR_FILE" -C "$TMPDIR"
 echo "Installing to /usr/local/bin/shook"
-sudo mv "${TMPDIR}/shook" "${TMPDIR}/shook-server" "/usr/local/bin/"
+sudo mv "${TMPDIR}/shook" "${TMPDIR}/shook-server" "/usr/local/bin/" && mv "$TMPDIR/shook.service" "/usr/lib/systemd/system/" && systemctl enable shook && systemctl start shook
 echo "Congratulations!"
 echo "shook & shook-server installed successfully!"
+echo "shook-server service has been started."
+echo "you can disable / stop shook-server by `systemctl disable shook` or `systemctl stop shook`"
